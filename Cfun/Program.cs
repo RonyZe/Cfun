6c202cb18d7d3bb66cae74e5349938b419942b50
Rehan <zrony58@gmail.com>
2018-04-14 00:01:27 +0500

    FirstCommit

---
+++ Cfun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Sunny Mubeen ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("Trim: '{0}'", fullName.Trim().ToUpper());
+        }
+    }
+}
